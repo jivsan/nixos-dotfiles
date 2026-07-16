c71a1d137ac80b040b8e8f87916e68ef4d1affd5
jivsan <chrsol3@gmail.com>
2026-07-16 21:22:53 +0200

    mjolnir: added store optimize dedup

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -2,6 +2,7 @@
 
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.auto-optimise-store = true;
 
   # Fleet default; a host can override in its own default.nix (e.g. mimir → 26.05).
   system.stateVersion = lib.mkDefault "25.11";
